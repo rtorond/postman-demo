965d172037a0e2d28ba9b6efb26683c9de3a0449
Romain Torond <romain.torond@ext.leroymerlin.fr>
2020-03-10 19:54:36 +0100

    feat: add orders services

---
+++ services/orders/server.js
@@ -0,0 +1,25 @@
+// Env variables
+process.env.NODE_ENV = process.env.NODE_ENV || "development";
+require('custom-env').env(process.env.NODE_ENV);
+
+// Imports
+const express = require('express');
+const bodyParser = require('body-parser');
+const low = require('lowdb');
+const FileAsync = require('lowdb/adapters/FileAsync');
+const Router = require('./src/Router');
+
+const PORT = process.env.PORT || 8080;
+const DB_FILE_PATH = process.env.NODE_ENV === 'production'
+    ? '/tmp/db.json'
+    : 'db.json';
+
+// Create server
+const app = express();
+app.use(bodyParser.json());
+
+// Create database instance and start server
+const adapter = new FileAsync(DB_FILE_PATH);
+low(adapter).then(Router(app)).then(() => {
+  app.listen(PORT, () => console.log('listening on port ' + PORT));
+});
